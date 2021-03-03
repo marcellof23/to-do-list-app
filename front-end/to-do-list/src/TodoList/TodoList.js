9aad31cc03897ab09def188cdc85442d7bf1dfd6
marcellof23 <01faria.marcello@gmail.com>
2021-03-04 03:03:07 +0700

    beresin front-end tinggal design

--- front-end/to-do-list/src/TodoList/TodoList.js
+++ front-end/to-do-list/src/TodoList/TodoList.js
@@ -1,10 +1,48 @@
 import React, { useState } from "react";
 import TodoForm from "../TodoForm/TodoForm";
+import Todo from "../Todo/Todo";
 function TodoList() {
 	const [todos, setTodos] = useState([]);
+
+	const addTodo = (todo) => {
+		if (!todo.text || /^\s*$/.test(todo.text)) return;
+
+		setTodos([todo, ...todos]);
+	};
+
+	const completeTodo = (id) => {
+		let updatedTodos = todos.map((todo) => {
+			if (todo.id === id) {
+				todo.isComplete = !todo.isComplete;
+			}
+			return todo;
+		});
+		setTodos(updatedTodos);
+	};
+
+	const removeTodo = (id) => {
+		const removed = [...todos].filter((td) => td.id !== id);
+		setTodos(removed);
+	};
+
+	const updateTodo = (id, newVal) => {
+		if (!newVal.text || /^\s*$/.test(newVal.text)) return;
+
+		setTodos((prevVal) =>
+			prevVal.map((item) => (item.id === id ? newVal : item))
+		);
+	};
+
 	return (
 		<div className="todo-app">
-			<TodoForm />
+			<h1>Hai lagi ngapain?</h1>
+			<TodoForm onSubmit={addTodo} />
+			<Todo
+				todos={todos}
+				completeTodo={completeTodo}
+				removeTodo={removeTodo}
+				updateTodo={updateTodo}
+			/>
 		</div>
 	);
 }
